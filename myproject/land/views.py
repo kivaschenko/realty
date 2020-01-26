65f1c480cd0067e718495a8fc2a323459a7da7ff
kivaschenko <civaschenko@yahoo.com>
2020-01-26 21:14:36 +0200

    fixed polygon scrpt

--- myproject/land/views.py
+++ myproject/land/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
+from django.core.mail import send_mail
 from .models import Land
 from .forms import LandForm, LandUpdateForm, ContactForm
 
