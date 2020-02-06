e9ee14013fac8335018902f2dbea33de2b4f9667
kivaschenko <konstantin.ivaschenko238@gmail.com>
2020-02-06 23:16:00 +0200

    modified settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -146,16 +146,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 # Add new email settings to send messages
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'konstantin.ivaschenko238@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Ki150797'
-# DEFAULT_FROM_EMAIL = "konstantin.ivaschenko238@gmail.com"
-# DEFAULT_TO_EMAIL = 'konstantin.ivaschenko238@gmail.com'
+EMAIL_USE_TLS = False
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = "info@cherkasyrealestate.org.ua"
+DEFAULT_TO_EMAIL = 'info@cherkasyrealestate.org.ua'
 
 # LEAFLET_CONFIG = {
 #     'SPATIAL_EXTENT': (31.44, 49.217, 32.47, 49.68), #<- xmin,ymin,xmax,ymax
