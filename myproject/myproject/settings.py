9d542470eeb93c6c9ec04faa00b33ecc93afee0d
kivaschenko <konstantin.ivaschenko238@gmail.com>
2020-02-07 14:59:32 +0200

    modified  setting EMAIL

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -154,8 +154,8 @@ EMAIL_HOST = 'localhost'
 EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL = "info@cherkasyrealestate.org.ua"
-DEFAULT_TO_EMAIL = 'info@cherkasyrealestate.org.ua'
+DEFAULT_FROM_EMAIL = "CherkasyRealEstate.Org.ua <info@cherkasyrealestate.org.ua>"
+# DEFAULT_TO_EMAIL = 'info@cherkasyrealestate.org.ua'
 
 # LEAFLET_CONFIG = {
 #     'SPATIAL_EXTENT': (31.44, 49.217, 32.47, 49.68), #<- xmin,ymin,xmax,ymax
